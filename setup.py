943d441b2ce0f7c8ea5efb37c3cf03989f6a4d41
Sven Grunewaldt <strayer@olle-orks.org>
2013-01-17 00:23:34 +0100

    Fix cx_Freeze and Qt "imageformats" folder

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
+import glob
+import os
+import shutil
 import sys
 from cx_Freeze import setup, Executable
+
+from PyQt4 import QtCore
+
+app = QtCore.QCoreApplication(sys.argv)
+qt_library_path = QtCore.QCoreApplication.libraryPaths()
+
+buildexe_options = {
+    'compressed' : True,
+    'optimize' : 2
+}
+
+imageformats_path = None
+for path in qt_library_path:
+    if os.path.exists(os.path.join(path, 'imageformats')):
+        imageformats_path = os.path.join(path, 'imageformats')
+        local_imageformats_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'imageformats')
+
+        buildexe_options['include_files'] = ['imageformats']
+
+        if not os.path.exists(local_imageformats_path):
+            os.mkdir(local_imageformats_path)
+        for file in glob.glob(os.path.join(imageformats_path, '*')):
+            shutil.copy(file, os.path.join(local_imageformats_path, os.path.basename(file)))
 
 setup(
     name='Bodenpython',
@@ -9,12 +35,10 @@ setup(
         Executable('serial_port_handler.py'),
         Executable('gui/main_window.py',
                    icon='gui/resources/app.ico',
-                   base="Win32GUI" if sys.platform == "win32" else None)
+                   base="Win32GUI" if sys.platform == "win32" else None,
+                   targetName='Bodenpython.exe' if sys.platform == "win32" else "bodenpython")
     ],
     options={
-        'build_exe' : {
-            'compressed' : True,
-            'optimize' : 2,
-        }
+        'build_exe' : buildexe_options
     }
 )
