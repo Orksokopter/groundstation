01946ec1ee6ab6302d201b3f0c2eabe69a4949eb
Sven Grunewaldt <strayer@olle-orks.org>
2013-03-07 21:28:13 +0100

    Remember last used serial port

--- gui/serial_port_dialog.py
+++ gui/serial_port_dialog.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from PyQt4 import QtGui
+from PyQt4.QtCore import QSettings
 from serial.tools import list_ports
 
 class SerialPortDialog(QtGui.QDialog):
     serialport_combobox = None
+    settings = None
 
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
@@ -31,11 +33,23 @@ class SerialPortDialog(QtGui.QDialog):
         buttonbox.rejected.connect(self.reject)
         buttonbox.accepted.connect(self.accept)
 
+        self.settings = QSettings('olle-orks.org', 'Bodenpython')
+
         comports = list_ports.comports()
         self.serialport_combobox.clear()
+        idx = 0
         for port in comports:
             self.serialport_combobox.addItem(port[1], port[0])
+
+            if port[0] == self.settings.value('last_selected_com_port'):
+                self.serialport_combobox.setCurrentIndex(idx)
+
+            idx += 1
         self.serialport_combobox.setFixedWidth(300)
+
+    def accept(self):
+        self.settings.setValue('last_selected_com_port', self.get_selected_serial_port())
+        super(SerialPortDialog, self).accept()
 
     def get_selected_serial_port(self):
         return self.serialport_combobox.itemData(self.serialport_combobox.currentIndex())
