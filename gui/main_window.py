8da4a8c345d92269ff658e2889da3cd63cdd0fcb
Sven Grunewaldt <strayer@olle-orks.org>
2015-05-20 21:54:21 +0200

    Use communicators in GUI

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,18 +1,16 @@
-# coding=utf-8
 import logging
-from queue import Queue
 import sys
+
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QVBoxLayout
-import serial
 from serial.serialutil import SerialException
+
 from gui.serial_port_dialog import SerialPortDialog
 from gui.widgets import PingPongWidget, MessageListWidget
 from messages import BaseMessage, PingMessage, NopMessage, ConfirmationMessage
-from serial_port_handler import SerialRead, SerialWrite
-
-import gui.resources_rc
+from protocol.EmulatedCommunicator import EmulatedCommunicator
+from protocol.SerialPortCommunicator import SerialPortCommunicator
 
 
 class MainWindow(QtWidgets.QWidget):
@@ -23,11 +21,7 @@ class MainWindow(QtWidgets.QWidget):
         QtWidgets.QApplication.setWindowIcon(QtGui.QIcon(':/icons/app-icon'))
         self.setWindowTitle('Mikrokopter Bodenpython')
 
-        self.selected_serial_port = None
-
-        self.serial_reader = None
-        self.serial_writer = None
-        self.serial_writer_queue = None
+        self.communicator = None
 
         self.list_widget = None
         self.pingpong_widget = None
@@ -59,7 +53,7 @@ class MainWindow(QtWidgets.QWidget):
         logging.debug('Selected port {}'.format(selected_port))
 
         try:
-            self.selected_serial_port = serial.Serial(selected_port, 57600)
+            self.communicator = SerialPortCommunicator(selected_port)
         except SerialException as e:
             QtWidgets.QMessageBox.critical(
                 self,
@@ -69,27 +63,16 @@ class MainWindow(QtWidgets.QWidget):
             QtCore.QTimer.singleShot(0, self.close)
             return
 
-        # This needs to be set so the threads may have a chance to abort
-        self.selected_serial_port.timeout = 1
+        self.communicator.received_message.connect(self.reader_received_message)
+        self.communicator.sent_message.connect(self.writer_sent_message)
 
-        self.serial_writer_queue = Queue()
-        self.serial_reader = SerialRead(self.selected_serial_port)
-        self.serial_writer = SerialWrite(self.selected_serial_port, self.serial_writer_queue)
-        self.serial_reader.connect_to_writer(self.serial_writer)
-
-        self.serial_reader.received_message.connect(self.reader_received_message)
-        self.serial_writer.sent_message.connect(self.writer_sent_message)
-
-        self.serial_reader.start()
-        self.serial_writer.start()
-
-        self.serial_writer_queue.put(PingMessage())
+        self.communicator.send_message(PingMessage())
 
         self.init_gui()
 
     def init_gui(self):
         self.list_widget = MessageListWidget(self)
-        self.pingpong_widget = PingPongWidget(self.serial_writer_queue, self)
+        self.pingpong_widget = PingPongWidget(self.communicator, self)
 
         layout = QVBoxLayout()
         layout.addWidget(self.list_widget)
@@ -98,21 +81,11 @@ class MainWindow(QtWidgets.QWidget):
         self.setLayout(layout)
 
     def closeEvent(self, event):
-        if self.selected_serial_port and self.selected_serial_port.isOpen():
+        if self.communicator is not None:
             logger = logging.getLogger()
 
-            logger.debug("Aborting threads")
-            self.serial_reader.abort()
-            self.serial_writer.abort()
-
-            logger.debug("Waiting for threads to stop")
-            self.serial_reader.wait()
-            self.serial_writer.wait()
-
-            logger.debug("Closing serial port")
-            self.selected_serial_port.close()
-
-            logger.debug("Serial port closed")
+            logger.debug("Stopping communicator")
+            self.communicator.stop()
 
         event.accept()
 
